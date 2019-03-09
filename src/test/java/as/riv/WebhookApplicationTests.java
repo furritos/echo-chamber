f74a5e0297b7c79605507d82d954daaf59011902
Carlos Rivas <crivas@yodlee.com>
2019-03-09 11:30:07 -0800

    Renamed WebhookIoApplicationTests to WebhookApplicationTests.

--- src/test/java/as/riv/WebhookApplicationTests.java
+++ src/test/java/as/riv/WebhookApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WebhookIoApplicationTests {
+public class WebhookApplicationTests {
 
 	@Test
 	public void contextLoads() {
