0e281b1423bd260e0e561ad47e9fe65d569b1d81
Carlos <carlos@riv.as>
2019-03-24 23:01:34 -0700

    Added transactional SQL statement to destructively delete keys

--- src/main/java/as/riv/repository/APIKeyRepository.java
+++ src/main/java/as/riv/repository/APIKeyRepository.java
@@ -1,11 +1,16 @@
 package as.riv.repository;
 
+import java.util.Date;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import as.riv.model.APIKey;
 
 public interface APIKeyRepository extends CrudRepository<APIKey, UUID> {
+
+	@Transactional
+	void deleteByExpiresBefore(Date expiryDate);
 
 }
