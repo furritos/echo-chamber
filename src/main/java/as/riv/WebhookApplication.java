76b95906ed31a2ef09e962fbefface0d9a81b36d
Carlos <carlos@riv.as>
2019-03-24 23:03:28 -0700

    Enabled application for scheduling events

--- src/main/java/as/riv/WebhookApplication.java
+++ src/main/java/as/riv/WebhookApplication.java
@@ -3,9 +3,11 @@ package as.riv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.map.repository.config.EnableMapRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableMapRepositories
+@EnableScheduling
 public class WebhookApplication {
 
 	public static void main(String[] args) {
